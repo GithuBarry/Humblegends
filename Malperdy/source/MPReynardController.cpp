9940639136815426630d554b87ca80b51e42dc18
shh95 <shh95@cornell.edu>
2022-02-23 15:47:42 -0500

    Resolve xcode

---
+++ Malperdy/source/MPReynardController.cpp
@@ -0,0 +1,9 @@
+//
+//  MPReynardController.cpp
+//  Malperdy
+//
+//  Created by Spencer Hurst on 2/23/22.
+//  Copyright © 2022 Humblegends. All rights reserved.
+//
+
+#include "MPReynardController.hpp"
---
+++ Malperdy/source/MPReynardController.hpp
@@ -0,0 +1,25 @@
+//
+//  MPReynardController.hpp
+//  Malperdy
+//
+//  Created by Spencer Hurst on 2/23/22.
+//  Copyright © 2022 Humblegends. All rights reserved.
+//
+
+#ifndef MPReynardController_hpp
+#define MPReynardController_hpp
+
+#include <stdio.h>
+#include <cugl/cugl.h>
+#include "MPReynardModel.h"
+
+class Reynard_Controller {
+    
+    
+    
+    
+};
+
+
+
+#endif /* MPReynardController_hpp */
