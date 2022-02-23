4d688d70ac6d1d3c211fea215ee8facee716ccb1
Barry Wang <zw545@cornell.edu>
2022-02-23 15:56:43 -0500

    Revert "Resolve xcode"
    This reverts commit 9940639136815426630d554b87ca80b51e42dc18.

--- Malperdy/source/MPReynardController.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MPReynardController.cpp
-//  Malperdy
-//
-//  Created by Spencer Hurst on 2/23/22.
-//  Copyright © 2022 Humblegends. All rights reserved.
-//
-
-#include "MPReynardController.hpp"
