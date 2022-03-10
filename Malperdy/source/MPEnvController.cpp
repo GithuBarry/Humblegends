c98bfab42d87145399d11d41d8c2428680586d14
irSelin <isabelselin@gmail.com>
2022-03-10 00:01:11 -0500

    Rough in locking rooms w/Reynard
    - improve commenting for EnvController
    - reorganize MPEnvController.h
    - remove debug logs from EnvController
    
    - add a section for helper functions
    - add helper function containsReynard()
    - write pseudocode implementation for containsReynard()
    - update selectRoom() and swapWithSelected() to call containsReynard
    
    - move appearance-changing code for deselecting into deselectRoom()
    - update selectRoom() and swapWithSelected() to call deselectRoom() when deselecting

--- Malperdy/source/MPEnvController.cpp
+++ Malperdy/source/MPEnvController.cpp
@@ -5,7 +5,7 @@
 //  Controller for the house of Malperdy
 //
 //  Authors: Jordan Selin
-//  Copyright � 2022 Humblegends. All rights reserved.
+//  Copyright (c) 2022 Humblegends. All rights reserved.
 //  Version: 2/28/22
 //
 
@@ -18,44 +18,92 @@ EnvController::EnvController(){
 	_toSwap = Vec2(-1, -1);
 }
 
-
+/*
+* Selects the room at the given location
+* Selection is for the purpose of being swapped with another room in swapWithSelected
+* Does not currently check if the room is swappable
+*
+* @param coords     the coordinates of the selection in worldspace
+*
+* @return true if room was successfully selected, and false otherwise
+*/
 bool EnvController::selectRoom(Vec2 coords){
 	Vec2 room1 = _grid->worldSpaceToRoom(coords);
-	if (room1.x==-1 || room1.y == -1) {
-		_toSwap = Vec2(-1, -1);
-		return false;
-	}else{
-		_grid->getRoom(room1)->setColor(Color4::RED);
-	}
-	_toSwap = room1;
-	CULog("Room selected: (%f, %f)", _toSwap.x, _toSwap.y);
+	bool isValidRoom = room1.x != -1 && room1.y != -1;
+	bool isOccupied = containsReynard(room1);
 
-	return true;
+	if (!isValidRoom || isOccupied) {
+		deselectRoom();
+		return false;
+	}
+	else{
+		_grid->getRoom(room1)->setColor(Color4::RED);
+		_toSwap = room1;
+		return true;
+	}
 }
 
+/*
+* Swaps the room at the given location with the selected room
+* If room at given location is the selected room, deselects the room
+* Does not currently check if the room is swappable
+*
+* @param coords     the coordinates of the selection in worldspace
+*
+* @return true if rooms were successfully swapped
+* @return false if room was the same as selected room & is now deselected
+* @return false if no swap occurred
+*/
 bool EnvController::swapWithSelected(Vec2 coords){
 	if (!hasSelected()) {
 		return false;
 	}
+
 	Vec2 room2 = _grid->worldSpaceToRoom(coords);
-	if (room2.x == -1 || room2.y == -1) {
+	bool isValidRoom = room2.x != -1 && room2.y != -1;
+	bool isOccupied = containsReynard(room2) || containsReynard(_toSwap);
+
+	if (!isValidRoom || isOccupied) {
 		return false;
 	}
-	_grid->getRoom(_toSwap)->setColor(Color4::WHITE);
 	if (room2.x == _toSwap.x && room2.y == _toSwap.y) {
-		CULog("Tried to swap with itself");
-		_toSwap = Vec2(-1, -1);
+		deselectRoom();
 		return false;
 	}
 
 	bool success = _grid->swapRooms(_toSwap, room2);
 	if (success) {
-		CULog("Swapping with: (%f, %f)", room2.x, room2.y);
-		_toSwap = Vec2(-1, -1);
+		//Sloppy code, fix when refactoring UI updates
+		_toSwap = room2;
+		deselectRoom();
 	}
 	return success;
 }
 
+/* Deselects the currently selected room, if one is selected */
 void EnvController::deselectRoom(){
-	_toSwap = nullptr;
+	if (_toSwap.x != -1 && _toSwap.y != -1) {
+		_grid->getRoom(_toSwap)->setColor(Color4::WHITE);
+	}
+	_toSwap = Vec2(-1, -1);
+}
+
+#pragma mark Helper Functions
+/*
+* Checks whether Reynard is inside the indicated room
+*
+* @param pos    the position Reynard is currently at
+* @param size   Reynard's size
+* @param room   the row and column of the room to check
+* 
+* @return true if Reynard is inside the given room
+*/
+bool EnvController::containsReynard(Vec2 room) {
+	//TODO: write actual implementation
+	//calculate Reynard's corners by his position & size
+	//for each corner
+		//call GridModel to see the room that corner is in
+		//if that room is the room we're checking
+			//return true
+	return false;
 }
