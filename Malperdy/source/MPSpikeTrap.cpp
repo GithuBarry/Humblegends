c22102596f8528ead301c4d3101bc7ecdf3e2a97
shh95 <shh95@cornell.edu>
2022-03-13 23:58:30 -0400

    Spike Trap Nearly Done

---
+++ Malperdy/source/MPSpikeTrap.cpp
@@ -0,0 +1,9 @@
+//
+//  MPSpikeTrap.cpp
+//  RocketDemo
+//
+//  Created by Spencer Hurst on 3/13/22.
+//  Copyright © 2022 Cornell Game Design Initiative. All rights reserved.
+//
+
+
---
+++ Malperdy/source/MPSpikeTrap.hpp
@@ -0,0 +1,22 @@
+//
+//  MPSpikeTrap.hpp
+//  RocketDemo
+//
+//  Created by Spencer Hurst on 3/13/22.
+//  Copyright © 2022 Cornell Game Design Initiative. All rights reserved.
+//
+
+#ifndef MPSpikeTrap_hpp
+#define MPSpikeTrap_hpp
+
+//#include "MPTrapModel.h"
+#include "Mp"
+
+#include <stdio.h>
+
+class SpikeTrap : public TrapModel{
+    
+}
+
+
+#endif /* MPSpikeTrap_hpp */
