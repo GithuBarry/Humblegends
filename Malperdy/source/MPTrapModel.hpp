08dcd8227ad620d69dae9dbe5872e81d22b7a344
shh95 <shh95@cornell.edu>
2022-03-13 22:32:52 -0400

    Trap Basic Readded

--- Malperdy/source/MPTrapModel.hpp
+++ Malperdy/source/MPTrapModel.hpp
@@ -2,7 +2,7 @@
 //  MPTrapModel.hpp
 //  RocketDemo
 //
-//  Created by Spencer Hurst on 3/13/22.
+//  Created by Spencer Hurst on 3/7/22.
 //  Copyright © 2022 Cornell Game Design Initiative. All rights reserved.
 //
 
@@ -10,5 +10,35 @@
 #define MPTrapModel_hpp
 
 #include <stdio.h>
+#include <cugl/cugl.h>
+
+class TrapModel : public cugl::physics2::BoxObstacle{
+
+
+public:
+    enum class TrapState : int{
+        ACTIVATED,
+        DEACTIVATED
+    };
+    
+#pragma mark Destructors
+    /**
+     * Destroys this room, releasing all resources.
+     */
+    virtual ~TrapModel(void) { dispose(); }
+
+    /**
+     * Disposes all resources and assets of this room.
+     *
+     * Any assets owned by this object will be immediately released.  Once
+     * disposed, a room may not be used until it is initialized again.
+     */
+    void dispose();
+
+private:
+    TrapState _trapState;
+
+};
+
 
 #endif /* MPTrapModel_hpp */
