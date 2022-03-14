3651606324fc40f2acc1038e71ffb125a56f5a4c
shh95 <shh95@cornell.edu>
2022-03-13 21:11:27 -0400

    New Branch without Error

---
+++ Malperdy/source/MPTrapModel.cpp
@@ -0,0 +1,9 @@
+//
+//  MPTrapModel.cpp
+//  RocketDemo
+//
+//  Created by Spencer Hurst on 3/13/22.
+//  Copyright © 2022 Cornell Game Design Initiative. All rights reserved.
+//
+
+#include "MPTrapModel.hpp"
---
+++ Malperdy/source/MPTrapModel.hpp
@@ -0,0 +1,14 @@
+//
+//  MPTrapModel.hpp
+//  RocketDemo
+//
+//  Created by Spencer Hurst on 3/13/22.
+//  Copyright © 2022 Cornell Game Design Initiative. All rights reserved.
+//
+
+#ifndef MPTrapModel_hpp
+#define MPTrapModel_hpp
+
+#include <stdio.h>
+
+#endif /* MPTrapModel_hpp */
