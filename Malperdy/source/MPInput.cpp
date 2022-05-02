aa0e59c7d52dca83c1452de2ade385ac233b66a3
Evan Azari <eaa66@cornell.edu>
2022-05-02 15:37:59 -0400

    debug mobile input

--- Malperdy/source/MPInput.cpp
+++ Malperdy/source/MPInput.cpp
@@ -279,17 +279,23 @@ void InputController::update(float dt) {
         _touchTime += dt;
     }
     bool couldBeDash = didRelease() && _touchTime <= EVENT_SWIPE_TIME;
-    float xDist = (_touchEndPos - _touchStartPos).x;
+    float xDist = (_currPos - _touchStartPos).x;
     _dashLeftPressed = couldBeDash && xDist <= (-1) * EVENT_SWIPE_LENGTH;
     if (_dashLeftPressed){
-        CULog("MPInput dashed left");}
+        //CULog("MPInput dashed left");
+        
+    }
     _dashRightPressed = couldBeDash && xDist >= EVENT_SWIPE_LENGTH;
     if (_dashRightPressed){
         
-     CULog("MPInput dashed right");
+     //CULog("MPInput dashed right");
 }
     
     _jumpPressed = didRelease() && !_dashLeftPressed && !_dashRightPressed;
+    if (_jumpPressed){
+        
+     //CULog("MPInput jumped");
+}
 
 #endif
 
