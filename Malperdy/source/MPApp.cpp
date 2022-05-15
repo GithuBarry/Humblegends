76757b9c4a0b43320acc580a4bbc347d635fc90a
Barry Wang <33831597+GithuBarry@users.noreply.github.com>
2022-05-14 23:03:37 -0400

    Fix order and number for loading screen

--- Malperdy/source/MPApp.cpp
+++ Malperdy/source/MPApp.cpp
@@ -142,9 +142,10 @@ void Malperdy::update(float timestep) {
         _loading.update(0.01f);
     } else if (!_loaded) {
         _loading.dispose(); // Disables the input listeners in this mode
+        _gameplay.setMode(_loading.getMode());
         _gameplay.init(_assets);
         _loaded = true;
-        _gameplay.setMode(_loading.getMode());
+        
     } else {
         _gameplay.update(timestep);
     }
