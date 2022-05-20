053027a4cf9453d98bdd040da7185d1f534b9e6e
Kristina Gu <kcgu23@gmail.com>
2022-05-20 11:03:36 -0400

    Random physics obstacles fixed

--- Malperdy/source/MPGridModel.cpp
+++ Malperdy/source/MPGridModel.cpp
@@ -372,13 +372,15 @@ bool GridModel::init(shared_ptr<AssetManager> assets, float scale)
     _activeRegions->push_back(_regions->at(0));
 
     // Fill any empty spaces with solid rooms
-    for (int y = 0; y < _size.y; y++) {
-        for (int x = 0; x < _size.x; x++) {
-            // If there's no room there, put a solid one
-            if (getRoom(x, y) == nullptr)
-                _filler->push_back(RoomModel::alloc(x, y, "room_solid"));
-        }
-    }
+    //for (int y = 0; y < _size.y; y++) {
+    //    for (int x = 0; x < _size.x; x++) {
+    //        // If there's no room there, put a solid one
+    //        if (getRoom(x, y) == nullptr)
+    //            // Need to offset by grid origin to get GRID coordinates
+    //            setRoom(x, y, RoomModel::alloc(x + _originX, y + _originY, "room_solid"));
+    //            //_filler->push_back();
+    //    }
+    //}
 
     return this->scene2::SceneNode::init();
 };
@@ -644,13 +646,14 @@ void GridModel::calculatePhysicsGeometry()
             _physicsGeometry->at(row)->push_back(make_shared<vector<shared_ptr<physics2::PolygonObstacle>>>());
 
             room = getRoom(col, row);
+            if (room == nullptr) continue;
 
             // If there's no room here, pull the corresponding solid one from the fillers
             // This loop happens in the same order that the fillers were created, so we can just pop off
-            if (room == nullptr) {
+            /*if (room == nullptr) {
                 room = _filler->at(fillerInd);
                 fillerInd++;
-            }
+            }*/
 
             // Get pointers to PolygonNodes with the room's geometry
             shared_ptr<vector<shared_ptr<scene2::PolygonNode>>> geometry = room->getGeometry();
